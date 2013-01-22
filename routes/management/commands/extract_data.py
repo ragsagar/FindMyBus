ddaff038eb65a5844ed59c0d3162368103b831e4
ragsagar <ragsagar@gmail.com>
2013-01-23 00:26:41 +0530

    Updated the script to extract data from html files and store directly into database.

--- routes/management/commands/extract_data.py
+++ routes/management/commands/extract_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import NoArgsCommand
-from routes.models import Route
+from routes.models import Route, Stop
 from os.path import isfile
 from os.path import join as join_path
 from os import listdir
@@ -17,14 +17,22 @@ class Command(NoArgsCommand):
                        listdir(routes_dir) if isfile(join_path(routes_dir, file_))]
         for route_file in route_files:
             soup = BeautifulSoup(open(route_file).read())
-            route_stops = self.get_stops(soup)
+            route_stop_names = self.get_stops(soup)
+            route_stops = []
+            for stop_name in route_stop_names:
+                stop, created = Stop.objects.get_or_create(name=stop_name)
+                if created:
+                    print "New stop ", stop.name
+                route_stops.append(stop)
             route_num = self.get_route_number(soup)
-            print route_num
-            Route.objects.create(number=route_num,
+            route = Route.objects.create(number=route_num,
                                  name=self.get_route_name(soup),
-                                 stops=route_stops,
-                                 from_stop=route_stops[0],
-                                 to_stop=route_stops[-1])
+                                 from_stop=Stop.objects.get(name=route_stop_names[0]),
+                                 to_stop=Stop.objects.get(name=route_stop_names[-1]))
+            route.stops = route_stops
+            route.save()
+            print route.name, route.number
+            
 
     def get_stops(self, soup=None):
         """
@@ -32,14 +40,15 @@ class Command(NoArgsCommand):
         """
         soup = soup or self.soup
         stops_table = soup.findAll('table')[1].findAll('td')
-        return [stops_table[i].string for i in range(1, len(stops_table), 2)]
+        return [unicode(stops_table[i].string) for i in range(1, len(stops_table), 2)]
 
     def get_route_number(self, soup=None):
         """
         Get the route number.
         """
         soup = soup or self.soup
-        return soup.head.title.string.split('::')[1].split('(')[1][:-1]
+        return unicode(soup.head.title.string.split('::')[1].split('(')[1][:-1]
+                        )
         
 
     def get_route_name(self, soup=None):
@@ -47,4 +56,4 @@ class Command(NoArgsCommand):
         Get the title of the route.
         """
         soup = soup or self.soup
-        return soup.head.title.string.split('::')[1].split('(')[0]
+        return unicode(soup.head.title.string.split('::')[1].split('(')[0])
