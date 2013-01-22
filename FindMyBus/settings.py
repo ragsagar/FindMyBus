debf4ade08b03de06b2dadb306f1de92e1144e80
ragsagar <ragsagar@gmail.com>
2013-01-23 00:27:41 +0530

    Updated the settings to use psql db

--- FindMyBus/settings.py
+++ FindMyBus/settings.py
@@ -14,9 +14,9 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'punedata',                      # Or path to database file if using sqlite3.
-        'USER': 'ragsagar',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'busdb',                      # Or path to database file if using sqlite3.
+        'USER': 'bususer',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
