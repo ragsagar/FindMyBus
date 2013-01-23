f4d96bb2c45d6f9b08366217b6884537e30de313
ragsagar <ragsagar@gmail.com>
2013-01-24 00:10:57 +0530

    Changing some settings

--- FindMyBus/settings.py
+++ FindMyBus/settings.py
@@ -1,4 +1,4 @@
-# Django settings for dj_findmybus project.
+# Django settings for FindMyBus project.
 import os
 
 DEBUG = True
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'static_files')
+    os.path.join(PROJECT_ROOT, 'static_files'),
 )
 
 # List of finder classes that know how to find static files in
@@ -101,10 +101,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'dj_findmybus.urls'
+ROOT_URLCONF = 'FindMyBus.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'dj_findmybus.wsgi.application'
+WSGI_APPLICATION = 'FindMyBus.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
